0cc48786d7d61596891fc9675e66b114a3d6632f
Dave Manelski <davemanelski@gmail.com>
2025-12-07 15:49:43 -0800

    Initial SwiftUI macOS app project

---
+++ PromptBuilder/PromptBuilder/PromptBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptBuilderApp.swift
+//  PromptBuilder
+//
+//  Created by Dave Manelski on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PromptBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptBuilder/PromptBuilderTests/PromptBuilderTests.swift
@@ -0,0 +1,16 @@
+//
+//  PromptBuilderTests.swift
+//  PromptBuilderTests
+//
+//  Created by Dave Manelski on 12/7/25.
+//
+
+import Testing
+
+struct PromptBuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
